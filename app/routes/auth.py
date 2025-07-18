4e6decb419b030e7d1aba7969db354eb9e931b2f
OumaBon <mugahboniface@gmail.com>
2025-07-18 14:03:06 +0300

    feat(Login & Registation): updated

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,9 +1,9 @@
 from flask import jsonify, request,redirect, url_for
 from marshmallow import ValidationError
-
+from flask_jwt_extended import create_access_token
 
 from ..model import User 
-from ..schema import UserSchema 
+from ..schema import UserSchema, UserLogin
 from . import api 
 from ..import db
 
@@ -19,9 +19,8 @@ def register():
     except ValidationError as err:
         return jsonify({"errors": err.messages}), 400
     
-    
-    if User.query.filter_by(email=validated_data.get('email')).first:
-        redirect(url_for('login')), 302
+    if User.query.filter_by(email=validated_data.get('email')).first():
+        return jsonify({"error": "Email already exist"}),409
     else:
         user = User(**validated_data)
         user.set_password(data.get("password"))
@@ -33,11 +32,26 @@ def register():
 
 
 
-@api.route('/login', method=["POST"])
+@api.route('/login', methods=["POST"])
 def login():
-    pass
+    data = request.get_json()
+    login_schema = UserLogin()
     
+    try:
+        validated_data = login_schema.load(data)
+    except ValidationError as err:
+        return jsonify({"errors": err.messages}), 400
     
+    email = validated_data.get('email')
+    password = validated_data.get('password')
+    
+    user = User.query.filter_by(email=email).first()
+    if not user or not user.verify_password(password):
+        return jsonify({"error": "Invalid Email or Password"}),401
+    
+    access_token = create_access_token(identity=user.id)
+    return jsonify({"access_token": access_token, "user_id": user.id, "message": "Login Successful"})
+  
     
    
    
